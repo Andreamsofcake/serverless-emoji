45839e3d9455caa448fb86f509cf21ecbf5ca582
Andrea H Moulding <andrea.moulding@gmail.com>
2017-08-09 18:00:28 -0700

    readme

---
+++ handler.js
@@ -0,0 +1,31 @@
+'use strict';
+'use strict';
+console.log('Loading function');
+
+module.exports.emojiGenerate = (event, context, callback) => {
+    var emojis= [':‑)',':-]',':-3',':->','8-)',':-}'];
+    let emoji = emojis[Math.floor(Math.random()*emojis.length)];
+    console.log(emoji);
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: emoji,
+        input: event,
+      }),
+    };
+    callback(null, response);
+};
+// module.exports.hello = (event, context, callback) => {
+//   const response = {
+//     statusCode: 200,
+//     body: JSON.stringify({
+//       message: 'Go Serverless v1.0! Your function executed successfully!',
+//       input: event,
+//     }),
+//   };
+// 
+//   callback(null, response);
+// 
+//   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+//   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+// };
